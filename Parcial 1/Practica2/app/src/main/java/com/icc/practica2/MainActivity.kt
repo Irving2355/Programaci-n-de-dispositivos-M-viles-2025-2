200311c47720c8bdbdd94e54a229f9444ed279c6
Irving2355 <it235597@gmail.com>
2025-08-25 16:53:48 -0600

    Clase 25/08/2025

--- Parcial 1/Practica2/app/src/main/java/com/icc/practica2/MainActivity.kt
+++ Parcial 1/Practica2/app/src/main/java/com/icc/practica2/MainActivity.kt
@@ -1,47 +1,52 @@
 package com.icc.practica2
 
 import android.os.Bundle
-import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
+import android.util.Log
+import android.widget.Button
+import android.widget.CheckBox
+import android.widget.RadioButton
+import android.widget.RadioGroup
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.icc.practica2.ui.theme.Practica2Theme
+import androidx.appcompat.app.AppCompatActivity
 
-class MainActivity : ComponentActivity() {
+class MainActivity : AppCompatActivity() {
+    private lateinit var rgNivel: RadioGroup
+    private lateinit var rbBasico: RadioButton
+    private lateinit var rbIntermedio: RadioButton
+    private lateinit var rbAvanzado: RadioButton
+    private lateinit var cbKotlin: CheckBox
+    private lateinit var cbJetpack: CheckBox
+    private lateinit var cbFirebase: CheckBox
+    private lateinit var btnVerSeleccion: Button
+    private lateinit var tvResultado: TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        //pantallas fin a fin es opcional
         enableEdgeToEdge()
-        setContent {
-            Practica2Theme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+        setContentView(R.layout.activity_main)
+
+        rgNivel = findViewById(R.id.rgNivel)
+        rbBasico = findViewById(R.id.rbBasico)
+        rbIntermedio = findViewById(R.id.rbIntermedio)
+        rbAvanzado = findViewById(R.id.rbAvanzado)
+        cbKotlin = findViewById(R.id.cbKotlin)
+        cbJetpack = findViewById(R.id.cbJetpack)
+        cbFirebase = findViewById(R.id.cbFirebase)
+        btnVerSeleccion = findViewById(R.id.btnVerSeleccion)
+        tvResultado = findViewById(R.id.tvResultado)
+
+        // listener Radiogroup
+        rgNivel.setOnCheckedChangeListener {
+            _, checkedId ->
+            val nivel = when (checkedId){
+                R.id.rbBasico -> "Basico"
+                R.id.rbIntermedio -> "Intermedio"
+                R.id.rbAvanzado -> "Avanzado"
+                else -> "Ninguno"
             }
+            Log.i("Nivel","Nivel cambio a $nivel")
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    Practica2Theme {
-        Greeting("Android")
     }
 }
