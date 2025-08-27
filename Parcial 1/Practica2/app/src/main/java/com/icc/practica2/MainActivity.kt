240cd607058e1df57e1bc796c9c8088c6cbf9403
Irving2355 <it235597@gmail.com>
2025-08-27 14:34:54 -0600

    Clase 26/08/2025

--- Parcial 1/Practica2/app/src/main/java/com/icc/practica2/MainActivity.kt
+++ Parcial 1/Practica2/app/src/main/java/com/icc/practica2/MainActivity.kt
@@ -4,9 +4,11 @@ import android.os.Bundle
 import android.util.Log
 import android.widget.Button
 import android.widget.CheckBox
+import android.widget.CompoundButton
 import android.widget.RadioButton
 import android.widget.RadioGroup
 import android.widget.TextView
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 
@@ -24,7 +26,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         //pantallas fin a fin es opcional
-        enableEdgeToEdge()
+        //enableEdgeToEdge()
         setContentView(R.layout.activity_main)
 
         rgNivel = findViewById(R.id.rgNivel)
@@ -47,6 +49,36 @@ class MainActivity : AppCompatActivity() {
                 else -> "Ninguno"
             }
             Log.i("Nivel","Nivel cambio a $nivel")
+        }
+
+        //listener para checkbox
+        val checkListener = CompoundButton.OnCheckedChangeListener{
+            button, isChecked ->
+            Log.i("CheckBox","Checkbox ${button.text}: $isChecked")
+        }
+        cbKotlin.setOnCheckedChangeListener(checkListener)
+        cbJetpack.setOnCheckedChangeListener(checkListener)
+        cbFirebase.setOnCheckedChangeListener(checkListener)
+
+        //Accion del boton
+        btnVerSeleccion.setOnClickListener {
+            val nivel = when (rgNivel.checkedRadioButtonId){
+                R.id.rbBasico -> "Basico"
+                R.id.rbIntermedio -> "Intermedio"
+                R.id.rbAvanzado -> "Avanzado"
+                else -> "Ninguno"
+            }
+
+            //buildList + ifEmpty
+            val tecnologias = buildList {
+                if(cbKotlin.isChecked) add("Kotlin")
+                if(cbJetpack.isChecked) add("Jetpack")
+                if(cbFirebase.isChecked) add("Firebase")
+            }.joinToString { ", " }.ifEmpty { "Ninguna" }
+
+            val texto = "Nivel: $nivel\nTecnologías: $tecnologias"
+            tvResultado.text = texto
+            Toast.makeText(this,"Leido",Toast.LENGTH_SHORT).show()
         }
     }
 }
