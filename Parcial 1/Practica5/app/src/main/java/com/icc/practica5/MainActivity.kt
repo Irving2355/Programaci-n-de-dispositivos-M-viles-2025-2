4ea861ca01450b6e06f1f15f3463886edd7792cf
Irving2355 <it235597@gmail.com>
2025-09-19 16:45:12 -0600

    Clase 19/09/2025

--- Parcial 1/Practica5/app/src/main/java/com/icc/practica5/MainActivity.kt
+++ Parcial 1/Practica5/app/src/main/java/com/icc/practica5/MainActivity.kt
@@ -1,11 +1,29 @@
 package com.icc.practica5
 
 import android.os.Bundle
-import androidx.activity.ComponentActivity
+import android.widget.ArrayAdapter
+import androidx.appcompat.app.AppCompatActivity
+import androidx.media3.exoplayer.ExoPlayer
+import com.icc.practica5.databinding.ActivityMainBinding
 
-class MainActivity : ComponentActivity() {
+class MainActivity : AppCompatActivity(){
+
+    private lateinit var binding: ActivityMainBinding
+    private var audioPlayer : ExoPlayer? = null
+    private var videoPlayer : ExoPlayer? = null
+    private var options = listOf("Imagen","Audio","Video","Texto")
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val adapter = ArrayAdapter(this,
+            android.R.layout.simple_spinner_item,
+            options).also {
+                it.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        }
+        binding.spinner.adapter = adapter
 
     }
 }
