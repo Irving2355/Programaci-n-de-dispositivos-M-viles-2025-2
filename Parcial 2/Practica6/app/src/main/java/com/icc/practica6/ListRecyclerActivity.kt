00ebda383bb48f3fd59c350c26de4b570c634c48
Irving2355 <it235597@gmail.com>
2025-09-30 16:48:55 -0600

    Clase 30/09/2025

--- Parcial 2/Practica6/app/src/main/java/com/icc/practica6/ListRecyclerActivity.kt
+++ Parcial 2/Practica6/app/src/main/java/com/icc/practica6/ListRecyclerActivity.kt
@@ -1,4 +1,27 @@
 package com.icc.practica6
 
-class ListRecyclerActivity {
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.icc.practica6.databinding.ActivityrecyclerBinding
+
+class ListRecyclerActivity: AppCompatActivity() {
+    private lateinit var b: ActivityrecyclerBinding
+    private lateinit var adapter: ProductoAdapter
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        b = ActivityrecyclerBinding.inflate(layoutInflater)
+        setContentView(b.root)
+
+        adapter = ProductoAdapter(mutableListOf())
+        b.recyclerProductos.layoutManager = LinearLayoutManager(this)
+        b.recyclerProductos.adapter = adapter
+
+        cargarDesdeBack4App()
+    }
+
+    private fun cargarDesdeBack4App(){
+        
+    }
 }
