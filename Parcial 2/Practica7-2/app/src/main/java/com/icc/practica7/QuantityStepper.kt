42570bb881f477acf1f40b1a6270fe1d92c4ee18
Irving2355 <it235597@gmail.com>
2025-10-14 16:43:01 -0600

    Clase 14/10/2025

--- Parcial 2/Practica7-2/app/src/main/java/com/icc/practica7/QuantityStepper.kt
+++ Parcial 2/Practica7-2/app/src/main/java/com/icc/practica7/QuantityStepper.kt
@@ -1,13 +1,16 @@
 package com.icc.practica7
 
 import android.content.Context
+import android.graphics.PorterDuff
 import android.os.Handler
 import android.os.Looper
 import android.util.AttributeSet
+import android.view.HapticFeedbackConstants
 import android.view.LayoutInflater
 import android.widget.LinearLayout
 import android.widget.Toast
 import androidx.core.content.ContextCompat
+import com.google.android.material.button.MaterialButton
 import com.icc.practica7.databinding.ViewQuantityStepperBinding
 
 class QuantityStepper @JvmOverloads constructor(
@@ -48,67 +51,113 @@ class QuantityStepper @JvmOverloads constructor(
     private val handler = Handler(Looper.getMainLooper())
     private var repeating = false
 
-    init{
+    init {
         orientation = HORIZONTAL
 
         //leer atributios del xml/tema/estilos
-        val a = context.obtainStyledAttributes(attrs,R.styleable.QuantityStepper,
-            deafStyle,0)
-        min = a.getInt(R.styleable.QuantityStepper_qs_min,1)
-        max = a.getInt(R.styleable.QuantityStepper_qs_max,99)
-        step = a.getInt(R.styleable.QuantityStepper_qs_step,1)
-        qty = a.getInt(R.styleable.QuantityStepper_qs_initial,min)
-        unitPrice = a.getFloat(R.styleable.QuantityStepper_qs_unitPrice,
-            0f).toDouble()
+        val a = context.obtainStyledAttributes(
+            attrs, R.styleable.QuantityStepper,
+            deafStyle, 0
+        )
+        min = a.getInt(R.styleable.QuantityStepper_qs_min, 1)
+        max = a.getInt(R.styleable.QuantityStepper_qs_max, 99)
+        step = a.getInt(R.styleable.QuantityStepper_qs_step, 1)
+        qty = a.getInt(R.styleable.QuantityStepper_qs_initial, min)
+        unitPrice = a.getFloat(
+            R.styleable.QuantityStepper_qs_unitPrice,
+            0f
+        ).toDouble()
 
-        val iconMinus = a.getResourceId(R.styleable.QuantityStepper_qs_buttonIconMinus,
-            R.drawable.ic_remove_24)
+        val iconMinus = a.getResourceId(
+            R.styleable.QuantityStepper_qs_buttonIconMinus,
+            R.drawable.ic_remove_24
+        )
 
-        val iconPlus = a.getResourceId(R.styleable.QuantityStepper_qs_buttonIconPlus,
-            R.drawable.ic_add_24)
+        val iconPlus = a.getResourceId(
+            R.styleable.QuantityStepper_qs_buttonIconPlus,
+            R.drawable.ic_add_24
+        )
 
-        tintColor = a.getColor(R.styleable.QuantityStepper_qs_tint,
-            ContextCompat.getColor(context,R.color.teal_700))
+        tintColor = a.getColor(
+            R.styleable.QuantityStepper_qs_tint,
+            ContextCompat.getColor(context, R.color.teal_700)
+        )
 
-        if(attrs != null){
-            val ta = context.obtainStyledAttributes(attrs,
-                R.styleable.QuantityStepper)
-            min = ta.getInt(R.styleable.QuantityStepper_qs_min,
-                1)
-            max = ta.getInt(R.styleable.QuantityStepper_qs_max,
-                99)
-            step = ta.getInt(R.styleable.QuantityStepper_qs_step,
-                1)
-            qty = ta.getInt(R.styleable.QuantityStepper_qs_initial,
-                min)
-            ta.recycle()
+        cornerRadius = a.getDimension(
+            R.styleable.QuantityStepper_qs_cornerRadius,
+            16f
+        )
+
+        buttonSizePx = a.getDimensionPixelSize(
+            R.styleable.QuantityStepper_qs_buttonSize,
+            dp(40)
+        )
+
+        autoRepeatEnebled = a.getBoolean(
+            R.styleable.QuantityStepper_qs_autoRepeatEnabled,
+            true
+        )
+
+        vibrateOnLimit = a.getBoolean(
+            R.styleable.QuantityStepper_qs_vibrateOnLimit,
+            true
+        )
+        a.recycle()
+
+        //aplicar conos tamaños tintes y formas
+        styleButton(binding.btnMinus, iconMinus)
+        styleButton(binding.btnPlus, iconPlus)
+
+        //cantidad inicial en UI (intefaz de usuario)
+        binding.etQuantity.setText(qty.toString())
+
+        //accesibilad extra: hints y labels
+        binding.etQuantity.setOnFocusChangeListener{ v, hasFocus ->
+            if(!hasFocus) validateManualInput()
+
         }
 
-        binding.etQuantity.setText(qty.toString())
-        assignEvents()
+        //clicks y long-press
+        binding.btnMinus.setOnClickListener { nudge(-step) }
+        binding.btnPlus.setOnClickListener { nudge(step) }
+
+        //auto-repeat para long-press
+        setupAutoRepeat(binding.btnMinus, -step)
+        setupAutoRepeat(binding.btnPlus, step)
+
+        //assignEvents()
     }
 
-    private fun assignEvents(){
-        binding.btnMinus.setOnClickListener { changedBy(-step) }
-        binding.btnPlus.setOnClickListener { changedBy(step) }
-
-        binding.etQuantity.setOnFocusChangeListener{ _,hasFocus ->
-            if(!hasFocus){
-                val newVal = binding.etQuantity.text.toString()
-                    .toIntOrNull()?:qty
-                setQuantity(newVal, fromUser = true)
-            }
+    // estetica y tamaño
+    private fun styleButton(btn: MaterialButton,iconRes: Int){
+        btn.icon = ContextCompat.getDrawable(context,iconRes)
+        btn.layoutParams.apply {
+            width = buttonSizePx
+            height = buttonSizePx
         }
+        btn.iconTint?.setColorFilter(tintColor, PorterDuff.Mode.SRC_IN)
+        btn.backgroundTintList = ContextCompat.getColorStateList(
+            context, R.color.teal_700)
+        btn.cornerRadius = cornerRadius.toInt()
+    }
 
-        binding.btnMinus.setOnLongClickListener {
-            setQuantity(min, fromUser = true)
-            true
-        }
+    private fun dp(value: Int) = (resources.displayMetrics.density * value).toInt()
 
-        binding.btnPlus.setOnLongClickListener {
-            setQuantity(max, fromUser = true)
-            true
-        }
+    //Interacciones
+    private fun nudge(delta: Int){
+        val old = qty
+        setQuantityInternal(qty + delta, fromUser = true)
+        animateBump()
+        //feedback al limite
+        if(qty != old) performHapticFeedback(
+            HapticFeedbackConstants.KEYBOARD_TAP)
+        else if (vibrateOnLimit) performHapticFeedback(
+            HapticFeedbackConstants.REJECT)
+    }
+
+    private fun validateManualInput(){
+        val newVal = binding.etQuantity.text.toString().toIntOrNull()?: qty
+        setQuantityInternal(newVal,fromUser = true)
     }
 
     private fun changedBy(delta: Int){
