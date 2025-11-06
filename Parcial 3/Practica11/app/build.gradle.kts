46cdd2afa5256d225ea3f0851fa826b521bd38bd
Irving2355 <it235597@gmail.com>
2025-11-06 16:48:38 -0600

    Clase 06/11/2025

--- Parcial 3/Practica11/app/build.gradle.kts
+++ Parcial 3/Practica11/app/build.gradle.kts
@@ -38,6 +38,7 @@ android {
     }
     buildFeatures {
         compose = true
+        viewBinding = true
     }
 }
 
@@ -57,4 +58,9 @@ dependencies {
     androidTestImplementation(libs.androidx.compose.ui.test.junit4)
     debugImplementation(libs.androidx.compose.ui.tooling)
     debugImplementation(libs.androidx.compose.ui.test.manifest)
+
+    implementation ("com.google.android.material:material:1.12.0")
+    implementation ("androidx.activity:activity-ktx:1.9.1")
+    implementation ("androidx.constraintlayout:constraintlayout:2.1.4")
+    implementation ("androidx.appcompat:appcompat:1.7.0")
 }
